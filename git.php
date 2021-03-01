bd1eca40ae0fdf10f89f74f25417393fdbf56fd8
Hicham <hichamzarrouk1107@gmail.com>
2021-03-01 20:43:45 +0100

    Revert "add 'fatality' function"
    This reverts commit 8ae04c70673a55773509d9acbb8f768b742125ab.

--- git.php
+++ git.php
@@ -9,15 +9,9 @@ class Monster
 	const LIFE = 100;
 	const COUP_DE_POING = 20;
 	const COUP_DE_PIED = 35;
-	const FATALITY = 100;
 
 	public function coup_De_Pied($LIFE_ADVERSAIRE)
 	{
 		$LIFE_ADVERSAIRE = $LIFE_ADVERSAIRE - COUP_DE_PIED;
-	}
-
-	public function fatality()
-	{
-		$LIFE_ADVERSAIRE = $LIFE_ADVERSAIRE - FATALITY;
 	}
 }
