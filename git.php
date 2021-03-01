a9862478ea7e53e502999e47eeb0b6ccbe2181c9
Hicham <hichamzarrouk1107@gmail.com>
2021-03-01 20:32:31 +0100

    first commit

---
+++ git.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace Monster;
+/**
+ * 
+ */
+class Monster
+{
+	const LIFE = 100;
+	const COUP_DE_POING = 20;
+	const COUP_DE_PIED = 35;
+
+	public function coup_De_Pied($LIFE_ADVERSAIRE)
+	{
+		$LIFE_ADVERSAIRE = $LIFE_ADVERSAIRE - COUP_DE_PIED;
+	}
+}
