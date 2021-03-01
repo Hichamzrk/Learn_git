8ae04c70673a55773509d9acbb8f768b742125ab
Hicham <hichamzarrouk1107@gmail.com>
2021-03-01 20:43:45 +0100

    add 'fatality' function

--- git.php
+++ git.php
@@ -9,9 +9,15 @@ class Monster
 	const LIFE = 100;
 	const COUP_DE_POING = 20;
 	const COUP_DE_PIED = 35;
+	const FATALITY = 100;
 
 	public function coup_De_Pied($LIFE_ADVERSAIRE)
 	{
 		$LIFE_ADVERSAIRE = $LIFE_ADVERSAIRE - COUP_DE_PIED;
+	}
+
+	public function fatality()
+	{
+		$LIFE_ADVERSAIRE = $LIFE_ADVERSAIRE - FATALITY;
 	}
 }
