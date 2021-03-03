11b7c8048b793f1e30427cdd88c0a3e2c3b7095c
Hicham <hichamzarrouk1107@gmail.com>
2021-03-03 20:10:04 +0100

    change

--- git.php
+++ git.php
@@ -1,22 +1,20 @@
-<?php 
+<?php
 
 namespace Monster;
-/**
- * 
- */
+
 class Monster
 {
-	const LIFE = 100;
-	const COUP_DE_POING = 20;
-	const COUP_DE_PIED = 35;
+    const LIFE = 100;
+    const COUP_DE_POING = 20;
+    const COUP_DE_PIED = 35;
 
-	public function coup_De_Pied($LIFE_ADVERSAIRE)
-	{
-	$LIFE_ADVERSAIRE = $LIFE_ADVERSAIRE - COUP_DE_PIED;
-	}
+   public function coupDePied($LIFE_ADVERSAIRE)
+   {
+     $LIFE_ADVERSAIRE = $LIFE_ADVERSAIRE - self::COUP_DE_PIED;
+   }
 
-		public function coup_De_Poing($LIFE_ADVERSAIRE)
-	{
-		 $LIFE_ADVERSAIRE = $LIFE_ADVERSAIRE - COUP_DE_POING;
-	}
+   	public function coup_De_Poing($LIFE_ADVERSAIRE)
+   {
+   	 $LIFE_ADVERSAIRE = $LIFE_ADVERSAIRE - self::COUP_DE_POING;
+   }
 }
